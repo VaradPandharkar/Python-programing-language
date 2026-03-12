2cee31c25d2e6d30163d92a83d2ff91cce9e4fbc
varadpandharkar <varadpandharkar89@gmail.com>
2026-03-12 09:41:16 +0530

    Add files via upload

---
+++ if_statement.py
@@ -0,0 +1,18 @@
+"""
+age = int(input("Enter you age: "))
+if age >= 18:
+    print("You are signed up!")
+
+elif age < 0:
+    print("You Haven't born yet!")
+    
+else:
+    print("You must be 18+ to be sign up")
+
+"""
+
+response = input ("Would you like food? (Y/N): ")
+if response == "Y":
+    print("Have some food?")
+else: 
+    print("No food for you!")
