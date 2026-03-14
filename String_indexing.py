583bc2b26de55d24abcde1900bdf7e3986f40e62
VaradPandharkar <varadpandharkar89@gmail.com>
2026-03-14 18:06:21 +0530

    Add files via upload

---
+++ String_indexing.py
@@ -0,0 +1,10 @@
+credit_number = "1234-5678-9012-0234"
+# print (credit_number[12])
+# print(credit_number[0:4]
+# print(credit_number[5:9])
+# print(credit_number[5:])
+# print(credit_number[-3])
+# print(credit_number[::2])
+
+last_digit = credit_number[::-1]
+print(f"XXXX-XXXX-XXXX-{last_digit}")
